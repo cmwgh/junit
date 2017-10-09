839f2d3cfd38936014dfc1ad71b8e0e64ebdd691
Christopher White <cwhite01+github@gmail.com>
2017-10-09 11:54:10 +0200

    initial chapter

---
+++ applicatieMetTesten/src/test/java/be/vdab/valueobjects/JaarTest.java
@@ -0,0 +1,48 @@
+package be.vdab.valueobjects;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import org.junit.Test;
+
+
+
+public class JaarTest {
+	
+	
+	@Test
+	public void eenJaarDeelbaarDoor400IsEenSchrikkeljaar() {
+		assertTrue(new Jaar(2000).isSchrikkeljaar());
+	}
+	@Test
+	public void eenJaarDeelbaarDoor100IsGeenSchrikkeljaar() {
+		assertFalse(new Jaar(1900).isSchrikkeljaar());
+	}
+	@Test
+	public void eenJaarDeelbaarDoor4IsEenSchrikkeljaar() {
+		assertTrue(new Jaar(2012).isSchrikkeljaar());
+	}
+	@Test
+	public void eenJaarNietDeelbaarDoor4isGeenSchrikkeljaar() {
+		assertFalse(new Jaar(2015).isSchrikkeljaar());
+	}
+
+	
+	
+
+//	@Test
+//	public void eenJaarDeelbaarDoor400IsEenSchrikkeljaar(){
+//		Jaar jaar = new Jaar(2000);
+//		Assert.assertEquals(true, jaar.isSchrikkeljaar());
+//	}
+//	@Test
+//	public void eenJaarDeelbaarDoor100IsGeenSchrikkeljaar(){
+//		Assert.assertEquals(false, new Jaar(1900).isSchrikkeljaar());
+//	}
+//	@Test
+//	public void EenJaarDeelbaarDoor4IsEenSchrikkeljaar() {
+//		Assert.assertEquals(true, new Jaar(2012).isSchrikkeljaar());
+//	}
+//	@Test
+//	public void eenJaarNietDeelbaarDoor4IsGeenSchrikkeljaar(){
+//		Assert.assertEquals(false, new Jaar(2015).isSchrikkeljaar());
+//	}	
+}
