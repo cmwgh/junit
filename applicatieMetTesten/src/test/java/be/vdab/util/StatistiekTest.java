cddd3464c2927fb2f7a51cab6c4cac3ba0c6c35b
Christopher White <cwhite01+github@gmail.com>
2017-10-12 15:52:32 +0200

    ISBN

--- applicatieMetTesten/src/test/java/be/vdab/util/StatistiekTest.java
+++ applicatieMetTesten/src/test/java/be/vdab/util/StatistiekTest.java
@@ -1,34 +1,34 @@
 package be.vdab.util;
 
-import static org.junit.Assert.*;
-
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import java.math.BigDecimal;
 
-
 public class StatistiekTest {
-
 	@Test
 	public void hetGemiddeldeVan0en10is5() {
-		assertEquals(0, BigDecimal.valueOf(5).compareTo(Statistiek.getGemiddelde(
-				new BigDecimal[] {BigDecimal.ZERO, BigDecimal.TEN})));
+		assertEquals(0, BigDecimal.valueOf(5)
+				.compareTo(Statistiek.getGemiddelde(new BigDecimal[] { BigDecimal.ZERO, BigDecimal.TEN })));
 	}
+
 	@Test
 	public void hetGemiddeldeVanEenGetalIsDatGetal() {
 		BigDecimal enigGetal = BigDecimal.valueOf(1.23);
-		assertEquals(0, enigGetal.compareTo(
-				Statistiek.getGemiddelde(new BigDecimal[] { enigGetal })));
+		assertEquals(0, enigGetal.compareTo(Statistiek.getGemiddelde(new BigDecimal[] { enigGetal })));
 	}
+
 	@Test(expected = IllegalArgumentException.class)
-	public void hetGemiddeldeVanEenLegeVerzamelingKanNietBerekenen() {
-		Statistiek.getGemiddelde(new BigDecimal[] { });
+	public void hetGemiddeldeVanEenLegeVerzamelingKanJeNietBerekenen() {
+		Statistiek.getGemiddelde(new BigDecimal[] {});
 	}
+
 	@Test(expected = NullPointerException.class)
-	public void hetGemiddeldeVanNullKanJeNietBerekenen(){
+	public void hetGemiddeldeVanNullKanJeNietBerekenen() {
 		Statistiek.getGemiddelde(null);
 	}
+
 	@Test(expected = NullPointerException.class)
-	public void eenArrayElementMagNietNullBevatten(){
-		Statistiek.getGemiddelde(new BigDecimal[] { null});
+	public void eenArrayElementMagNietNullBevatten() {
+		Statistiek.getGemiddelde(new BigDecimal[] { null });
 	}
 }
