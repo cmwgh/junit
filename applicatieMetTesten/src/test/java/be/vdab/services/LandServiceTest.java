3462e047060b82501f1123412198ec627f531fdc
Christopher White <cwhite01+github@gmail.com>
2017-10-16 12:25:05 +0200

    Mock LandServiceTest

--- applicatieMetTesten/src/test/java/be/vdab/services/LandServiceTest.java
+++ applicatieMetTesten/src/test/java/be/vdab/services/LandServiceTest.java
@@ -3,18 +3,32 @@ import static org.junit.Assert.assertEquals;
 import java.math.BigDecimal;
 import org.junit.Before;
 import org.junit.Test;
+
+import be.vdab.entities.Land;
 import be.vdab.repositories.LandRepository;
 import be.vdab.repositories.LandRepositoryStub;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+
+
+
+
 
 public class LandServiceTest {
 	private LandRepository landRepository;
 	private LandService landService;
 	@Before public void before() {
-		landRepository = new LandRepositoryStub();
+		landRepository = mock(LandRepository.class);
+		when(landRepository.findOppervlakteAlleLanden()).thenReturn(20);
+		when(landRepository.read("B")).thenReturn(new Land("B", 5));
+		//landRepository = new LandRepositoryStub();
 		landService = new LandService(landRepository);
 	}
 	@Test public void findVerhoudingOppervlakteLandTovOppervlakteAlleLanden(){
 		assertEquals(0, BigDecimal.valueOf(0.25).compareTo(
 				landService.findVerhoudingOppervlakteLandTovOppervlakteAlleLanden("B")));
+		verify(landRepository).findOppervlakteAlleLanden();
+		verify(landRepository).read("B");
 	}
 }
